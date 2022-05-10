7bf1b61fa6306194205f1c47f7f33498cbc1cc49
tkrumrei <tkrumrei@uni-muenster.de>
2022-05-10 11:11:04 +0200

    Klasse Haltestelle

---
+++ haltestelle.js
@@ -0,0 +1,8 @@
+class Haltestelle {
+    constructor(nr, lbez, richtung, coordinates) {
+          this.nr = nr
+          this.lbez = lbez
+          this.richtung = richtung
+          this.coordinates = coordinates
+    }
+}
